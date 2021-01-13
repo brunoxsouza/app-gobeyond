0391f7fedaa272c801d8d4bfe6a9f8594303e80c
Bruno <souzabruno0110@gmail.com>
2021-01-13 20:21:11 -0300

    Created HeaderBar Component and added Home

--- src/AppNavigator.js
+++ src/AppNavigator.js
@@ -1,48 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { Image } from 'react-native';
-
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Image} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 
 import iconHome from './images/iconHome.png'
 import iconMyAccount from './images/iconMyAccount.png'
 
-import Home from '../src/modules/Home'
-import MyAccount from '../src/modules/MyAccount'
+import Home from './modules/Home'
+import MyAccount from './modules/MyAccount'
 
 const AppNavigator = () => {
-  const { Navigator, Screen } = createBottomTabNavigator()
-
+    const { Navigator, Screen } = createBottomTabNavigator()
 
   return (
     <NavigationContainer>
-      <Navigator
-        initialRouteName="Home"
-        headerMode="none"
-        tabBarOptions={{
-          activeTintColor: '#000',
-          inactiveTintColor: '#666',
-        }}
-      >
-        <Screen 
-        name ="Home" 
-        component={Home} 
-        options={{tabBarIcon: () => (
-          <Image resizeMode="contain" style={{ height: 25 }} source={iconHome} />
-        )
-        }}/>
-        <Screen 
-        name ="MyAccount" 
-        component={MyAccount}
-        options={{
-          title: 'Minha Conta',
-          tabBarIcon: () => (
-          <Image resizeMode="contain" style={{ height: 25 }} source={iconMyAccount} />
-        )
-        }}
-         />
-      </Navigator>
+        <Navigator
+          initialRouteName="Home"
+          headerMode="none"
+          tabBarOptions={{
+            activeTintColor: '#000',
+            inactiveTintColor: '#666'
+          }}
+        >
+            <Screen 
+              name="Home" 
+              component={Home} 
+              options={{
+                tabBarIcon: () => (
+                  <Image resizeMode="contain" style={{ height: 25 }} source={iconHome} />
+                )
+              }}  
+            />
+            <Screen name="MyAccount" component={MyAccount} 
+              options={{
+                title: 'Minha Conta',
+                tabBarIcon: () => (
+                  <Image resizeMode="contain" style={{ height: 25 }} source={iconMyAccount} />
+                )
+              }}  
+            />
+        </Navigator>
     </NavigationContainer>
   )
 }
--- src/modules/MyAccount/index.js
+++ src/modules/MyAccount/index.js
@@ -1,33 +1,33 @@
 import React from 'react';
-
-import { Text, View, TouchableOpacity, StyleSheet } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { useNavigation } from '@react-navigation/native'
 
 
 const MyAccount = () => {
-  const { navigate } = useNavigation()
+    const { navigate } = useNavigation()
 
   return (
-    <View style={{flex: 1, alignItems:'center'}}>
-      <Text>MyAccount</Text>
-      <View style={{flex: 1, alignItems:'center', justifyContent:'center'}}>
-      <TouchableOpacity style={styles.button} onPress={() => { navigate('Home') }} >
-        <Text style={styles.buttonText}>Ir para a Home</Text>
-      </TouchableOpacity>
+      <View style={{ flex: 1, alignItems: 'center'}}>
+          <Text>MyAccount</Text>
+          <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+          <TouchableOpacity style={styles.button} onPress={() => {  navigate('Home') }}>
+            <Text style={styles.buttonText}>Ir para Home</Text>
+          </TouchableOpacity>
+          </View>
       </View>
-    </View>
   )
+  
 }
 
 const styles = StyleSheet.create({
-  button: {
-    width: 150,
-    height: 50,
-    backgroundColor: '#4971fe',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  buttonText: {color: '#FFF'}
+    button: {
+        width: 150,
+        height: 50,
+      backgroundColor: '#4971fe',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    buttonText: { color: '#FFF'}
 })
 
 export default MyAccount
