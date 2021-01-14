a39a50452514c19621ba171ea4507b2335e7bb00
Bruno <souzabruno0110@gmail.com>
2021-01-14 13:10:05 -0300

    Created Components AdBanner and ProductBanner

--- src/modules/Home/index.js
+++ src/modules/Home/index.js
@@ -2,10 +2,14 @@ import React from 'react';
 import { SafeAreaView } from 'react-native';
 
 import HeaderBar from './components/HeaderBar'
+import AdBanner from './components/AdBanner'
+import ProductBanner from './components/ProductBanner';
 
 const Home = () => (
   <SafeAreaView style={{ flex: 1 }}>
     <HeaderBar />
+    <AdBanner />
+    <ProductBanner />
   </SafeAreaView>
 )
 
