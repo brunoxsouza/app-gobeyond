1517ecc532163b455a9ea55837693f5f77dd1ab5
Bruno <souzabruno0110@gmail.com>
2021-01-15 15:48:04 -0300

    Show Banners API on Home

--- src/modules/Home/index.js
+++ src/modules/Home/index.js
@@ -1,16 +1,43 @@
-import React from 'react';
-import { SafeAreaView } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { SafeAreaView, ScrollView } from 'react-native';
 
 import HeaderBar from './components/HeaderBar'
-import AdBanner from './components/AdBanner'
-import ProductBanner from './components/ProductBanner';
+import PrimaryBannerSlider from './components/PrimaryBannerSlider'
+import SecondBanner from './components/SecondBanner'
+import FourthBanner from './components/FourthBanner'
 
-const Home = () => (
-  <SafeAreaView style={{ flex: 1 }}>
-    <HeaderBar />
-    <AdBanner />
-    <ProductBanner />
-  </SafeAreaView>
-)
+import services from '../../services'
+
+const Home = () => {
+  const [banners, setBanners] = useState([])
+
+  useEffect(async () => {
+
+    var response
+
+    try {
+      response = await services.getBanners()
+    } catch (error) {
+      console.log(error)
+    }
+
+    const { data, status } = response
+
+    setBanners(data)
+  }, [])
+
+  console.log(banners)
+
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <HeaderBar />
+      <ScrollView>
+        <PrimaryBannerSlider data={banners[0]?.data || []} />
+        <SecondBanner data={banners[1]?.data || []} />
+        <FourthBanner data={banners[3]?.data || []} />
+      </ScrollView>
+    </SafeAreaView>
+  )
+}
 
 export default Home
