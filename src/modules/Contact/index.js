6e5634983661ef172fe22d2957b2646d68d979d5
Bruno <souzabruno0110@gmail.com>
2021-01-28 19:32:55 -0300

    Contact path changed and fix some components

---
+++ src/modules/Contact/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { SafeAreaView } from 'react-native';
+
+import Form from './components/Form'
+import Header from './components/Header'
+
+
+const Contact = () => {
+
+  return (
+    <SafeAreaView>
+    <Header />
+    <Form />
+    </SafeAreaView>
+  )
+  
+}
+
+export default Contact
